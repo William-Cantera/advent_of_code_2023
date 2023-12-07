60d3e24a74af0b05a1027d5b70f7b69f63c7cdea
William Cantera <wfcantera@gmail.com>
2023-12-07 01:09:21 -0500

    more problems, days 4/6

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,7 +1,9 @@
+use core::num;
 use std::collections::HashMap;
 use std::io::BufRead;
 use std::fs::File;
 use std::io::BufReader;
+use std::ptr::write_unaligned;
 use regex::Regex;
 
 const INPUT_PATH: &'static str = "C:\\Users\\wfcan\\advent_of_code_2023\\day1\\src\\input\\";
@@ -9,7 +11,11 @@ fn main() {
     //problem1();
     //problem2();
     //problem3();
-    problem4();
+    //problem4();
+    //problem7();
+    problem8();
+    //problem11();
+    //problem12();
 }
 
 fn problem1() {
@@ -203,4 +209,151 @@ fn problem4() {
     } else {
         println!("Error opening the file");
     }
+}
+
+fn problem7() {
+    let file_path = INPUT_PATH.to_string() + "day4_input1.txt";
+
+    if let Ok(file) = File::open(file_path) {
+        // Read the file line by line
+        let reader: BufReader<File> = BufReader::new(file); 
+        
+        let mut win_lst: Vec<Vec<i32>> = Vec::new();
+        let mut value_lst: Vec<Vec<i32>> = Vec::new();
+        for line in reader.lines() {
+            if let Ok(line) = line { 
+                let split: Vec<&str> = line.split(":").collect();
+                let card: Vec<&str> = split[1].split("|").collect();
+                //println!("{:?}", card);
+                let winning_numbers: Vec<&str> = card[0].split(" ").collect();
+                win_lst.push(winning_numbers.iter().map(|v| v.parse::<i32>().unwrap_or(-1)).collect());
+
+                let player_numbers: Vec<&str> = card[1].split(" ").collect();
+                value_lst.push(player_numbers.iter().map(|v| v.parse::<i32>().unwrap_or(-1)).collect());
+                //println!("{:?}", win_map);
+                //println!("{:?}", value_lst);
+            } 
+        }
+        let mut total_points: Vec<i128> = Vec::new();
+        for (index, values) in value_lst.iter().enumerate() {
+            let win_vals = win_lst.get(index).unwrap();
+            
+            let mut points: i128 = 0;
+            for val in values.iter() {
+                if *val == -1i32 {
+                    continue;
+                }
+                if win_vals.contains(val) {
+                    if points == 0 {
+                        points += 1;
+                    } else {
+                        points *= 2;
+                    }
+                    println!("{:?}, {:?}", val, points);
+                }
+            }
+            total_points.push(points);
+        }
+
+        println!("Problem 7 answer: {:?}", total_points.iter().sum::<i128>());
+    } else {
+        println!("Error opening the file");
+    }
+}
+
+fn problem8() {
+    let file_path = INPUT_PATH.to_string() + "day4_input1.txt";
+
+    if let Ok(file) = File::open(file_path) {
+        // Read the file line by line
+        let reader: BufReader<File> = BufReader::new(file); 
+        
+        let mut win_lst: Vec<Vec<i32>> = Vec::new();
+        let mut value_lst: Vec<Vec<i32>> = Vec::new();
+        for line in reader.lines() {
+            if let Ok(line) = line { 
+                let split: Vec<&str> = line.split(":").collect();
+                let card: Vec<&str> = split[1].split("|").collect();
+                let winning_numbers: Vec<&str> = card[0].split(" ").collect();
+                win_lst.push(winning_numbers.iter().map(|v| v.parse::<i32>().unwrap_or(-1)).collect());
+
+                let player_numbers: Vec<&str> = card[1].split(" ").collect();
+                value_lst.push(player_numbers.iter().map(|v| v.parse::<i32>().unwrap_or(-1)).collect());
+            } 
+        }
+        let mut copies: Vec<i128> = Vec::new();
+        for _ in 0..207 {    
+            copies.push(1);        
+        }
+
+        
+        for (index, values) in value_lst.iter().enumerate() {            
+            let win_vals = win_lst.get(index).unwrap();
+            let mut num_matches: i32 = 0;
+            println!("win vals {:?}", win_vals);
+            println!("val vals {:?}", values);
+
+           
+            for val in values.iter() {
+                if *val == -1 {
+                    continue;
+                }
+                if win_vals.contains(val) {
+                    num_matches += 1;
+                }
+            }
+
+            //println!("natches {:?}", num_matches);
+    
+            for c in 0..num_matches {
+                let card_num = c as usize + index + 1;
+                //println!("{:?}", copies[index]);
+                copies[card_num] += copies[index];                               
+            }
+            //println!("{:?}", copies);
+
+            //println!("--------------------------------");
+
+            
+        }
+
+
+        println!("Problem 8 answer: {:?}", copies.iter().sum::<i128>());
+    } else {
+        println!("Error opening the file");
+    }
+}
+
+fn problem11() {
+    let race_map: HashMap<&str, i32> = HashMap::from([
+        ("53", 313),
+        ("89", 1090),
+        ("76", 1214),
+        ("98", 1201),
+    ]);
+    let mut win_counts: Vec<i32> = Vec::new();
+    for (key, value) in race_map {
+        println!("{}: {}", key, value);
+        let total_time: i32 = key.parse::<i32>().unwrap();
+        let mut possible_wins: i32 = 0;
+        for i in 0..total_time {
+            if i * (total_time-i) > value {
+                possible_wins += 1;
+            }
+        }
+        win_counts.push(possible_wins);
+    }
+    println!("Problem 11 solution: {}", win_counts.iter().product::<i32>());
+}
+
+fn problem12() {
+    let time: i128 = 53897698;
+    let distance: i128 = 313109012141201;
+    let mut possible_wins: i128 = 0;
+    for i in 0..time {
+        if i * (time-i) > distance {
+            possible_wins += 1;
+        }
+    }
+    println!("Problem 12 solution: {}", possible_wins);
 }
